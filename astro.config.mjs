597d6a5bf27c5e0857beaa7514046b2a74b8599f
Guillermo Vera Ghanain <guilleverag@gmail.com>
2024-11-15 18:01:32 -0500

    reverse astro-i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import astroI18next from "astro-i18next";
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),astroI18next()],
+  integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
-  }
+  },
 });

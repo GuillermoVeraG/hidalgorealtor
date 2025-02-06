a7d67af64395ef83f985e489ff5169686983cd3b
Guillermo Vera Ghanain <guilleverag@gmail.com>
2025-02-06 11:58:27 -0500

    fix routes language in result & overview

--- src/utils/menu.ts
+++ src/utils/menu.ts
@@ -5,3 +5,18 @@ export function isMenuActive(pathname: string, menu: string) {
     return menu == "/" || menu == "/es/";
   }
 }
+
+export function isCompActive(pathname: string, menu: string) {
+  if (
+    menu == "buyer" &&
+    (pathname.includes("buyer/result/") || pathname.includes("buyer/overview/"))
+  ) {
+    return false;
+  }
+
+  if (pathname != "/" && pathname != "/es/") {
+    return menu != "/" && pathname.includes(menu);
+  } else {
+    return menu == "/" || menu == "/es/";
+  }
+}
